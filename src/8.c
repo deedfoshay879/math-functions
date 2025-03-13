0e3b0bc72b165ea10317955b768cd83a56331b68
deedfoshay879 <deedfoshay879@gmail.com>
2025-03-13 10:20:42 +0000

    Add 8.c

---
+++ src/8.c
@@ -0,0 +1,19 @@
+int add(int x, int y) {
+    return x + y;
+}
+
+int subtract(int x, int y) {
+    return x - y;
+}
+
+int multiply(int x, int y) {
+    return x * y;
+}
+
+int divide(int x, int y) {
+    if (y == 0) {
+        return INT_MAX;
+    } else {
+        return x / y;
+    }
+}
