fecc080c0ca55083e388c2ba7827b639d86e16db
deedfoshay879 <deedfoshay879@gmail.com>
2025-03-30 09:43:43 +0000

    Add 21.c

---
+++ src/21.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    double x = 5.0;
+    printf("The square of %f is %.2f\n", x, pow(x, 2));
+    printf("The cube of %f is %.2f\n", x, pow(x, 3));
+}
