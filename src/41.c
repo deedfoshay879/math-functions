e1732f25c7d7ee1e57fe1c3c2c778d36b0dc03a5
deedfoshay879 <deedfoshay879@gmail.com>
2025-05-23 04:44:44 +0000

    Add 41.c

---
+++ src/41.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Function to perform subtraction
+int subtract(int a, int b) {
+    return a - b;
+}
+
+// Main function
+int main() {
+    printf("Subtraction: %d - %d = %d\n", 10, 5, 5);
+    return 0;
+}
